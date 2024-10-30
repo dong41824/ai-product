6e2ffc88ed189ae7d79043fcf8ad1f15835a00dc
dong41824 <2270770373@qq.com>
2024-10-30 09:22:08 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -3,12 +3,12 @@ st.title("AI大模型应用产品网")
 col, col1 = st.columns(2)
 with col:
     st.image("https://tse1-mm.cn.bing.net/th/id/OIP-C.h9dmt0VA30NmskyQaDDm7wHaFj?w=230&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7,width=300")
-    flag = st.button("会员中心")
+    flag = st.button("问答系统")
     if flag:
         st.switch_page("pages/demo03.py")
 with col1:
     st.image("https://tse1-mm.cn.bing.net/th/id/OIP-C.h9dmt0VA30NmskyQaDDm7wHaFj?w=230&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7,width=300")
-    flag1 = st.button("会员中心二部")
+    flag1 = st.button("生图系统")
     if flag1:
         st.switch_page("pages/textToImage.py")
 # c1,c2,c3,c4,c5=st.columns(5)
